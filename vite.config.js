f4bc4a66e16bacd1ee7c04532d693324b3e2447c
Deva092002 <92011465+Deva092002@users.noreply.github.com>
2025-02-28 17:41:00 +0530

    Updated vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+const isGithubPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  base: "/Devashish-Portfolio/",
+  base: isGithubPages ? "/Devashish-Portfolio/" : "/",
 })
