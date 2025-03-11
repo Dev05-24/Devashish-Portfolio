fbbde0f5ff575872260ca65d4e52062b97ce1599
Devashish Kunwar <deckunwar@gmail.com>
2025-03-12 00:33:30 +0530

    Update Project.jsx added new project taskmanager

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,6 +4,7 @@ const projectsData = [
   { id: 1,title: "Currency-Convertor",img: "./images/currency-convertor.png",link: "https://currency-convertor-ochre-nu.vercel.app/",},
   { id: 2, title: "Weather-App", img: "./images/weather-app.png", link: "https://weather-app-rosy-eta-51.vercel.app/",},
   { id: 3, title: "Newsify", img: "./images/newsify.png", link: "https://newsify-eta.vercel.app/"},
+  { id: 4, title: "TaskManager", img: "./images/TaskManager.png", link: "https://task-managing-cyan.vercel.app/"},
 ];
 const Projects = () => {
   return (
